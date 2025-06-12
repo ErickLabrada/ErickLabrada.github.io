47639023fe59730ac08e457e3951ef02c23d16ed
Erick Labrada <Erick.labrada233380@potros.itson.edu.mx>
2025-06-12 11:28:39 -0700

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: 'https://ericklabrada.github.io',
-  base: "ericklabrada.github.io",
+  site: "https://ericklabrada.github.io/",
+  base: "/", // important
+  outDir: "dist",
 });
